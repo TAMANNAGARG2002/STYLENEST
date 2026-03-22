05dd6c534296062d2e7fec3a319cc9403d0a5562
namjain9 <namjain9@publicisgroupe.net>
2026-03-22 17:56:29 +0530

    added sample chat app

--- Django-Doc_app/editor/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    """Render the main editor page.
-
-    This view serves a single-page rich text editor. All editing logic is client-side.
-    """
-    return render(request, "editor/index.html")
