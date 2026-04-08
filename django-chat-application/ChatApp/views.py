894642eb6c0fb281d0bec4f47e2b80a11cb5ce2e
tamanna garg <tamanna.garg@thoughts2binary.com>
2026-04-08 10:56:05 +0530

    change cron

--- django-chat-application/ChatApp/views.py
+++ django-chat-application/ChatApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 
 def redirector(request):
     if request.user.is_authenticated:
-        # return redirect('chat:lobby')
+        return redirect('chat:lobby')
     else:
         # return redirect('account:login')
     
